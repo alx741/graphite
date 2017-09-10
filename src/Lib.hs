547c78f3ae5f455422d34410e7349c44e92a9a77
Daniel Campoverde <alx@sillybytes.net>
2017-09-09 19:16:37 -0500

    Basic Edges type classes

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,26 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE FlexibleInstances #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import Data.Hashable
+
+type VertexHash = Int
+
+class Edge e where
+    vertex1 :: e -> VertexHash
+    vertex2 :: e -> VertexHash
+
+class Edge a => Arc a where
+    fromVertex :: a -> VertexHash
+    toVertex :: a -> VertexHash
+
+class Edge e => WeightedEdge e where
+    edgeWeight :: e -> Double
+
+instance Hashable v => Edge (v, v) where
+    vertex1 = hash . fst
+    vertex2 = hash . snd
+
+instance Hashable v => Arc (v, v) where
+    fromVertex = vertex1
+    toVertex = vertex2
