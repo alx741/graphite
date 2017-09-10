37667a03866b3c4f3f6024b8e5a1aaf2a83be7c0
Daniel Campoverde <alx@sillybytes.net>
2017-09-10 15:43:07 -0500

    Eq instances for Edges and Arcs

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,12 +10,63 @@ import qualified Data.HashMap.Lazy as HM
 data Edge v a
     = Edge v v a
     -- | WeightedEdge Double v v a
-    deriving (Eq, Show)
+    deriving (Show)
 
 data Arc v a
     = Arc v v a
     -- | WeightedArc Double v v a
-    deriving (Eq, Show)
+    deriving (Show)
+
+-- | To 'Edge's are equal if they point to the same vertices, regardless of the
+-- | direction
+instance (Eq v, Eq a) => Eq (Edge v a) where
+    (Edge v1 v2 a) == (Edge v1' v2' a') =
+        (a == a')
+        && (v1 == v1' && v2 == v2')
+        || (v1 == v2' && v2 == v1')
+
+-- | To 'Arc's are equal if they point to the same vertices, and the directions
+-- | is the same
+instance (Eq v, Eq a) => Eq (Arc v a) where
+    (Arc v1 v2 a) == (Arc v1' v2' a') = (a == a') && (v1 == v1' && v2 == v2')
+
+class IsEdge e where
+    -- | Get Edges from an association list of vertices and their links
+    fromLinks :: [(v, Links v a)] -> [e v a]
+
+    -- | Get the vertices of an Edge ignoring its attributes
+    toTuple :: e v a -> (v, v)
+
+instance IsEdge Arc where
+    fromLinks = linksToArcs
+    toTuple = arcToTuple
+
+instance IsEdge Edge where
+    fromLinks = linksToEdges
+    toTuple = edgeToTuple
+
+-- | Get 'Arc's from an association list of vertices and their links
+linksToArcs :: [(v, Links v a)] -> [Arc v a]
+linksToArcs ls = concat $ fmap toArc ls
+    where
+        toArc :: (v, Links v a) -> [Arc v a]
+        toArc (fromV, links) = fmap (\(v, a) -> Arc fromV v a) (HM.toList links)
+
+-- | Get 'Edge's from an association list of vertices and their links
+-- FIXME: this one is incorrect for Edge
+linksToEdges :: [(v, Links v a)] -> [Edge v a]
+linksToEdges ls = concat $ fmap toEdge ls
+    where
+        toEdge :: (v, Links v a) -> [Edge v a]
+        toEdge (fromV, links) = fmap (\(v, a) -> Edge fromV v a) (HM.toList links)
+
+-- | Get the vertices of an 'Arc' ignoring its attributes
+arcToTuple :: Arc v a -> (v, v)
+arcToTuple (Arc fromV toV _) = (fromV, toV)
+
+-- | Get the vertices of an 'Edge' ignoring its attributes
+edgeToTuple :: Edge v a -> (v, v)
+edgeToTuple (Edge v1 v2 _) = (v1, v2)
 
 class EdgeAttr a where
     edgeWeight :: a -> Maybe Double
@@ -68,7 +119,7 @@ vertices = HM.keys
 
 -- | Retrieve the 'Arc's of a 'DiGraph'
 arcs :: forall v a . (Hashable v, Eq v) => DiGraph v a -> [Arc v a]
-arcs g = linksToArcs $ zip vs links
+arcs g = fromLinks $ zip vs links
     where
         vs :: [v]
         vs = vertices g
@@ -77,7 +128,8 @@ arcs g = linksToArcs $ zip vs links
 
 -- | Retrieve the 'Arc's of a 'DiGraph' as tuples, ignoring its attributes
 arcs' :: (Hashable v, Eq v) => DiGraph v a -> [(v, v)]
-arcs' g = fmap arcToTuple $ arcs g
+arcs' g = fmap toTuple $ arcs g
+-- arcs' g = fmap arcToTuple $ arcs g
 
 -- | Insert a link directed to *v* with attribute *a*
 -- | If the connnection already exists, the attribute is replaced
@@ -87,17 +139,6 @@ insertLink v attr m = HM.insert v attr m
 -- | Get the links for a given vertex
 getLinks :: (Hashable v, Eq v) => v -> DiGraph v a -> Links v a
 getLinks = HM.lookupDefault HM.empty
-
--- | Get 'Arc's from an association list of vertices and their links
-linksToArcs :: [(v, Links v a)] -> [Arc v a]
-linksToArcs ls = concat $ fmap toArc ls
-    where
-        toArc :: (v, Links v a) -> [Arc v a]
-        toArc (fromV, links) = fmap (\(v, a) -> Arc fromV v a) (HM.toList links)
-
--- | Get the vertices of an 'Arc' ignoring its attributes
-arcToTuple :: Arc v a -> (v, v)
-arcToTuple (Arc fromV toV _) = (fromV, toV)
 
 
 myGraph :: DiGraph Int (Double, String)
