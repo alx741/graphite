adca9b1278e96886072530a717a59e17beebfbca
Daniel Campoverde <alx@sillybytes.net>
2017-09-10 16:50:29 -0500

    Setup tests

---
+++ test/Data/Graph/TypesSpec.hs
@@ -0,0 +1,10 @@
+module Data.Graph.TypesSpec where
+
+import Test.Hspec
+import Test.QuickCheck
+
+spec :: Spec
+spec = do
+    describe "Undirected Ege" $ do
+        it "something" $ do
+            pending
