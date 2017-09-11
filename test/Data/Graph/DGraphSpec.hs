2ba4314c2cefd767888a0a7fffd37acbe03aa978
Daniel Campoverde <alx@sillybytes.net>
2017-09-11 17:43:07 -0500

    Test vertex and arc existence

--- test/Data/Graph/DGraphSpec.hs
+++ test/Data/Graph/DGraphSpec.hs
@@ -4,10 +4,23 @@ import Test.Hspec
 import Test.QuickCheck
 
 import Data.Graph.DGraph
+import Data.Graph.Types
 
 spec :: Spec
 spec = do
     describe "Directed Graph (DGraph)" $ do
+        it "Can tell if a vertex exists" $ property $ do
+            let g = insertVertex 1 empty :: DGraph Int ()
+            let g' = insertVertex 2 empty :: DGraph Int ()
+            containsVertex g 1 `shouldBe` True
+            containsVertex g' 1 `shouldBe` False
+
+        it "Can tell if an arc exists" $ property $ do
+            let g = insertArc (1 --> 2) empty :: DGraph Int ()
+            let g' = insertArc (2 --> 1) empty :: DGraph Int ()
+            containsArc g (1 --> 2) `shouldBe` True
+            containsArc g' (1 --> 2) `shouldBe` False
+
         it "Increments its order when a new vertex is inserted" $ property $
             \g v -> (not $ g `containsVertex` v) ==> order g + 1 == order (insertVertex v (g :: DGraph Int ()))
         it "Increments its size when a new arc is inserted" $ property $
