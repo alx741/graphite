e1bedc81e8de4765539f600144b715a187714c03
Daniel Campoverde <alx@sillybytes.net>
2017-09-12 19:34:44 -0500

    Graph Edge insertion

--- src/Data/Graph/Graph.hs
+++ src/Data/Graph/Graph.hs
@@ -25,9 +25,33 @@ insertVertex v = hashMapInsert v HM.empty
 -- | Every 'Edge' adjacent to this vertex is also removed
 -- FIXME
 -- removeVertex :: (Hashable v, Eq v) => v -> Graph v e -> Graph v e
--- removeVertex v g = HM.delete v $ foldl' (flip removeArc) g $ incidentArcs g v
+-- removeVertex v g = HM.delete v $ foldl' (flip removeEdge) g $ incidentEdges g v
 
 -- | @O(m*log n)@ Insert a many vertices into a 'Graph'
 -- | New vertices are inserted and already contained vertices are left untouched
 insertVertices :: (Hashable v, Eq v) => [v] -> Graph v e -> Graph v e
 insertVertices vs g = foldl' (flip insertVertex) g vs
+
+-- | @O(log n)@ Insert an undirected 'Edge' into a 'Graph'
+-- | The involved vertices are inserted if don't exist. If the graph already
+-- | contains the Edge, its attribute is updated
+insertEdge :: (Hashable v, Eq v) => Edge v e -> Graph v e -> Graph v e
+insertEdge (Edge v1 v2 edgeAttr) g = link v2 v1 $ link v1 v2 g'
+    where
+        g' = insertVertices [v1, v2] g
+        link fromV toV = HM.adjust (insertLink toV edgeAttr) fromV
+
+-- | @O(m*log n)@ Insert many directed 'Edge's into a 'Graph'
+-- | Same rules as 'insertEdge' are applied
+-- insertEdges :: (Hashable v, Eq v) => [Edge v e] -> Graph v e -> Graph v e
+-- insertEdges as g = foldl' (flip insertEdge) g as
+
+
+-- | Insert a link directed to *v* with attribute *a*
+-- | If the connnection already exists, the attribute is replaced
+insertLink :: (Hashable v, Eq v) => v -> a -> Links v a -> Links v a
+insertLink = HM.insert
+
+-- | Get the links for a given vertex
+getLinks :: (Hashable v, Eq v) => v -> Graph v e -> Links v e
+getLinks = HM.lookupDefault HM.empty
