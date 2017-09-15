8692f9ade524b7d6ece224011847135a86182e82
Daniel Campoverde <alx@sillybytes.net>
2017-09-15 18:40:45 -0500

    Define getDegreeSequence

--- src/Data/Graph/Graph.hs
+++ src/Data/Graph/Graph.hs
@@ -207,8 +207,10 @@ degreeSequence = DegreeSequence . reverse . sort . filter (>0)
 
 -- | Get the 'DegreeSequence' of a simple 'Graph'
 -- | If the graph is not @simple@ (see 'isSimple') the result is Nothing
-getDegreeSequence :: Graph v e -> Maybe DegreeSequence
-getDegreeSequence = undefined
+getDegreeSequence :: (Hashable v, Eq v) => Graph v e -> Maybe DegreeSequence
+getDegreeSequence g
+    | (not . isSimple) g = Nothing
+    | otherwise = Just $ degreeSequence $ degrees g
 
 -- | Tell if a 'DegreeSequence' is a Graphical Sequence
 -- | A Degree Sequence is a @Graphical Sequence@ if a corresponding 'Graph' for
