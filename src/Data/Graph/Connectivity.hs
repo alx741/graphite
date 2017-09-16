f0b6821a8c8b93e8900d26ecd9888158e273d15a
Daniel Campoverde <alx@sillybytes.net>
2017-09-16 18:18:33 -0500

    Directly reachable vertices

--- src/Data/Graph/Connectivity.hs
+++ src/Data/Graph/Connectivity.hs
@@ -3,8 +3,37 @@
 
 module Data.Graph.Connectivity where
 
-import Data.Graph.UGraph
+import Data.Hashable
+
 import Data.Graph.DGraph
+import Data.Graph.Types
+import Data.Graph.UGraph
+
+-- | Tell if two vertices of a graph are connected
+-- | Two vertices are @connected@ if it exists a path between them
+areConnected :: Graph g => g v e -> v -> v -> Bool
+areConnected = undefined
+
+someG :: UGraph Int ()
+someG = insertEdges [(1 <-> 2), (2 <-> 3), (3 <-> 4), (5 <-> 6)] empty
+
+-- | All the vertices that are directly reachable from a particular vertex
+-- | A vertex is @directly reachable@ to other if there is a path of exaclty one
+-- | edge between them
+directlyReachableVertices :: (Graph g, Hashable v, Eq v) => g v e -> v -> [v]
+directlyReachableVertices g v = filter
+    (\v' -> containsEdgePair g (v, v'))
+    (adjacentVertices g v)
+
+-- | Tell if two vertices of a 'UGraph' are disconnected
+-- | Two vertices are @disconnected@ if it doesn't exist a path between them
+areDisconnected :: UGraph v e -> v -> v -> Bool
+areDisconnected = undefined
+
+-- | Retrieve all the unreachable vertices of a 'UGraph'
+-- | The @unreachable vertices@ are those with no adjacent 'Edge's
+unreachableVertices :: UGraph v e -> [v]
+unreachableVertices = undefined
 
 -- | Tell if a 'UGraph' is connected
 -- | An Undirected Graph is @connected@ when there is a path between every pair
@@ -18,21 +47,6 @@ isConnected = undefined
 -- TODO: An edgeles graph with two or more vertices is disconnected
 isDisconnected :: UGraph v e -> Bool
 isDisconnected = not . isConnected
-
--- | Tell if two vertices of a 'UGraph' are connected
--- | Two vertices are @connected@ if it exists a path between them
-areConnected :: UGraph v e -> v -> v -> Bool
-areConnected = undefined
-
--- | Tell if two vertices of a 'UGraph' are disconnected
--- | Two vertices are @disconnected@ if it doesn't exist a path between them
-areDisconnected :: UGraph v e -> v -> v -> Bool
-areDisconnected = undefined
-
--- | Retrieve all the unreachable vertices of a 'UGraph'
--- | The @unreachable vertices@ are those with no adjacent 'Edge's
-unreachableVertices :: UGraph v e -> [v]
-unreachableVertices = undefined
 
 -- | Tell if a 'DGraph' is weakly connected
 -- | A Directed Graph is @weakly connected@ if the equivalent undirected graph
