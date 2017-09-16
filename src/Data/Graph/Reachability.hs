b747a89ad3faadb33043636c286f12b0671671a4
Daniel Campoverde <alx@sillybytes.net>
2017-09-16 17:53:10 -0500

    Remove Reachability module

--- src/Data/Graph/Reachability.hs
+++
@@ -1,8 +0,0 @@
-module Data.Graph.Reachability where
-
-import Data.Hashable
-
-import Data.Graph.Types
-
-canReach :: (Graph g, Hashable v, Eq v) => g v e -> v -> v -> Bool
-canReach g v1 v2 = containsEdgePair g (v1, v2)
