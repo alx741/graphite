d49556ee21893899021eb2d6886a4a776176e947
Daniel Campoverde <alx@sillybytes.net>
2017-09-27 22:01:18 -0500

    Declare graph regularity functions

--- src/Data/Graph/Morphisms.hs
+++ src/Data/Graph/Morphisms.hs
@@ -1,6 +1,8 @@
 module Data.Graph.Morphisms where
 
+import Data.Graph.DGraph
 import Data.Graph.Types
+import Data.Graph.UGraph
 
 -- | Tell if two graphs are isomorphic
 -- TODO: check first: same number of vertices, same number of edges
@@ -9,3 +11,14 @@ areIsomorphic = undefined
 
 isomorphism :: Graph g => g v e -> g v' e' -> (v -> v')
 isomorphism = undefined
+
+-- | Tell if a 'UGraph' is regular
+-- | An undirected graph is regular if each vertex has the same degree
+isURegular :: UGraph v e -> Bool
+isURegular = undefined
+
+-- | Tell if a 'DGraph' is regular
+-- | A directed graph is regular if each vertex has the same indigree and
+-- | outdegree
+isDRegular :: DGraph v e -> Bool
+isDRegular = undefined
