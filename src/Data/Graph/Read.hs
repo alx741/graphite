9b87246698e8484717f7d73694617aee643aa389
Daniel Campoverde <alx@sillybytes.net>
2017-09-29 19:51:32 -0500

    UGraph reading from CSV file

--- src/Data/Graph/Read.hs
+++ src/Data/Graph/Read.hs
@@ -1,8 +1,32 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Data.Graph.Read where
 
-import Data.Csv as CSV
+import Data.ByteString.Lazy as BS
+import Data.Csv             as CSV
+import Data.Hashable
+import Data.Vector          as V hiding (fromList)
 
+import Data.Graph.Types
 import Data.Graph.UGraph
 
-csvToUGraph :: FilePath -> UGraph Int ()
-csvToUGraph = undefined
+-- | Read a 'UGraph' from a CSV file
+-- | The line "1,2,3,4" translates to the list of edges
+-- | "(1 <-> 2), (1 <-> 3), (1 <-> 4)"
+csvToUGraph :: (Hashable v, Eq v, FromField v)
+    => FilePath
+    -> IO (Either String (UGraph v ()))
+csvToUGraph fp = do
+    content <- BS.readFile fp
+    let dec = decode NoHeader content
+    case dec of
+        Left err  -> return $ Left err
+        Right vec -> return $ Right $ fromList $ toEdges $ V.toList vec
+
+    where
+        toEdges :: [[v]] -> [Edge v ()]
+        toEdges ns = Prelude.concat $ fmap nodeEdges ns
+
+        nodeEdges :: [v] -> [Edge v ()]
+        nodeEdges []     = []
+        nodeEdges (n:ns) = fmap (\n' -> Edge n n' ()) ns
