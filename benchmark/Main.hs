46b10dabdca36996e9b165707a35d598c0178843
Daniel Campoverde <alx@sillybytes.net>
2017-10-02 18:44:13 -0500

    Benchmark graph generation

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -2,4 +2,14 @@ module Main where
 
 import Criterion.Main
 
-main = defaultMain []
+import Data.Graph.Generation
+import Data.Graph.Types
+import Data.Graph.UGraph
+
+main = defaultMain
+    [ bgroup "generation"
+        [ bench "gnp_100" $ whnfIO (erdosRenyi 100 0.3 :: IO (UGraph Int ()))
+        , bench "gnp_500" $ whnfIO (erdosRenyi 500 0.3 :: IO (UGraph Int ()))
+        , bench "gnp_1000" $ whnfIO (erdosRenyi 1000 0.3 :: IO (UGraph Int ()))
+        ]
+    ]
