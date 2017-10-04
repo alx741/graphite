f9ff6720c023dc98a5321275e89f2eb960f70436
Daniel Campoverde <alx@sillybytes.net>
2017-10-04 16:56:35 -0500

    Benchmark 'edges'

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -2,19 +2,38 @@ module Main where
 
 import Criterion.Main
 
+import Control.DeepSeq
 import Data.Graph.Generation
+
+import Data.Graph.DGraph
 import Data.Graph.Types
 import Data.Graph.UGraph
 
 main = do
-    ug100 <- erdosRenyi 100 0.3 :: IO (UGraph Int ())
-    ug500 <- erdosRenyi 500 0.3 :: IO (UGraph Int ())
+    ug100 <- force <$> erdosRenyi 100 0.3 :: IO (UGraph Int ())
+    ug500 <- force <$> erdosRenyi 500 0.3 :: IO (UGraph Int ())
+    ug1000 <- force <$> erdosRenyi 1000 0.3 :: IO (UGraph Int ())
+
+    dg100 <- force <$> erdosRenyi 100 0.3 :: IO (DGraph Int ())
+    dg500 <- force <$> erdosRenyi 500 0.3 :: IO (DGraph Int ())
+    dg1000 <- force <$> erdosRenyi 1000 0.3 :: IO (DGraph Int ())
 
     defaultMain
         [ bgroup "generation"
-            [ bench "gnp_100" $ whnfIO (erdosRenyi 100 0.3 :: IO (UGraph Int ()))
-            , bench "gnp_500" $ whnfIO (erdosRenyi 500 0.3 :: IO (UGraph Int ()))
-            , bench "gnp_1000" $ whnfIO (erdosRenyi 1000 0.3 :: IO (UGraph Int ()))
+            [ bench "gnp_100" $ nfIO (erdosRenyi 100 0.3 :: IO (UGraph Int ()))
+            , bench "gnp_500" $ nfIO (erdosRenyi 500 0.3 :: IO (UGraph Int ()))
+            , bench "gnp_1000" $ nfIO (erdosRenyi 1000 0.3 :: IO (UGraph Int ()))
+            , bench "gnp_5000" $ nfIO (erdosRenyi 2000 0.3 :: IO (UGraph Int ()))
+            ]
+
+        , bgroup "edges"
+            [ bench "edges_ug100" $ nf edges ug100
+            , bench "edges_ug500" $ nf edges ug500
+            , bench "edges_ug1000" $ nf edges ug1000
+
+            , bench "arcs_dg100" $ nf arcs dg100
+            , bench "arcs_dg500" $ nf arcs dg500
+            , bench "arcs_dg1000" $ nf arcs dg1000
             ]
 
         , bgroup "properties"
