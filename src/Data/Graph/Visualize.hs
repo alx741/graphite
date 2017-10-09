7e98611c3a926284ecc247eadd9fab86a167084c
Daniel Campoverde <alx@sillybytes.net>
2017-10-09 18:29:42 -0500

    Visualize module styling

--- src/Data/Graph/Visualize.hs
+++ src/Data/Graph/Visualize.hs
@@ -20,7 +20,7 @@ plotUGraph g = runGraphvizCanvas Sfdp (toUndirectedDot g) Xlib
 
 -- | Plot an undirected 'UGraph' to a PNG image file
 plotUGraphPng :: (Show e) => UGraph Int e -> FilePath -> IO FilePath
-plotUGraphPng g fp = addExtension (runGraphvizCommand Sfdp $ toUndirectedDot g) Png fp
+plotUGraphPng g = addExtension (runGraphvizCommand Sfdp $ toUndirectedDot g) Png
 
 -- | Plot a directed 'DGraph'
 plotDGraph :: (Show e) => DGraph Int e -> IO ()
@@ -28,16 +28,16 @@ plotDGraph g = runGraphvizCanvas Sfdp (toDirectedDot g) Xlib
 
 -- | Plot a directed 'DGraph' to a PNG image file
 plotDGraphPng :: (Show e) => DGraph Int e -> FilePath -> IO FilePath
-plotDGraphPng g fp = addExtension (runGraphvizCommand Sfdp $ toDirectedDot g) Png fp
+plotDGraphPng g = addExtension (runGraphvizCommand Sfdp $ toDirectedDot g) Png
 
 labeledNodes :: (Graph g, Show v) => g v e -> [(v, String)]
-labeledNodes g = fmap (\v -> (v, show v)) $ vertices g
+labeledNodes g = (\v -> (v, show v)) <$> vertices g
 
 labeledEdges :: (Hashable v, Eq v, Show e) => UGraph v e -> [(v, v, String)]
-labeledEdges g = fmap (\(Edge v1 v2 attr) -> (v1, v2, show attr)) $ edges g
+labeledEdges g = (\(Edge v1 v2 attr) -> (v1, v2, show attr)) <$> edges g
 
 labeledArcs :: (Hashable v, Eq v, Show e) => DGraph v e -> [(v, v, String)]
-labeledArcs g = fmap (\(Arc v1 v2 attr) -> (v1, v2, show attr)) $ arcs g
+labeledArcs g = (\(Arc v1 v2 attr) -> (v1, v2, show attr)) <$> arcs g
 
 toUndirectedDot :: (Show e) => UGraph Int e -> DotGraph Int
 toUndirectedDot g = graphElemsToDot params (labeledNodes g) (labeledEdges g)
