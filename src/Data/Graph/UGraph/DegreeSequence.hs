b7b06a31807c0ef78d677c6b6e0ed21280d371eb
Daniel Campoverde <alx@sillybytes.net>
2017-10-21 19:47:23 -0500

    Fix Data.Graph.UGraph.DegreeSequence haddocks

--- src/Data/Graph/UGraph/DegreeSequence.hs
+++ src/Data/Graph/UGraph/DegreeSequence.hs
@@ -1,4 +1,19 @@
 module Data.Graph.UGraph.DegreeSequence where
+    (
+    DegreeSequence
+
+    -- * Construction
+    , degreeSequence
+    , getDegreeSequence
+
+    -- * Queries
+    , isGraphicalSequence
+    , isDirectedGraphic
+    , holdsHandshakingLemma
+
+    -- * Graph generation
+    , fromGraphicalSequence
+    ) where
 
 import Data.List (reverse, sort)
 
@@ -7,28 +22,29 @@ import Data.Hashable
 import Data.Graph.Types
 import Data.Graph.UGraph
 
--- | The Degree Sequence of a simple 'UGraph' is a list of degrees of vertices
--- | in a graph
--- | Use 'degreeSequence' to construct a valid Degree Sequence
+-- | The Degree Sequence of a simple 'UGraph' is a list of degrees of the
+-- vertices in the graph
+--
+-- Use 'degreeSequence' to construct a valid Degree Sequence
 newtype DegreeSequence = DegreeSequence { unDegreeSequence :: [Int]}
     deriving (Eq, Ord, Show)
 
--- | Construct a 'DegreeSequence' from a list of degrees
--- | Negative degree values are discarded
+-- | Construct a 'DegreeSequence' from a list of degrees. Negative degree values
+-- get discarded
 degreeSequence :: [Int] -> DegreeSequence
 degreeSequence = DegreeSequence . reverse . sort . filter (>0)
 
--- | Get the 'DegreeSequence' of a simple 'UGraph'
--- | If the graph is not @simple@ (see 'isSimple') the result is Nothing
+-- | Get the 'DegreeSequence' of a simple 'UGraph'. If the graph is not @simple@
+-- (see 'isSimple') the result is Nothing
 getDegreeSequence :: (Hashable v, Eq v) => UGraph v e -> Maybe DegreeSequence
 getDegreeSequence g
     | (not . isSimple) g = Nothing
     | otherwise = Just $ degreeSequence $ degrees g
 
 -- | Tell if a 'DegreeSequence' is a Graphical Sequence
--- | A Degree Sequence is a @Graphical Sequence@ if a corresponding 'UGraph' for
--- | it exists.
--- | Use the Havel-Hakimi algorithm
+--
+-- A Degree Sequence is a @Graphical Sequence@ if a corresponding 'UGraph' for
+-- it exists. Uses the Havel-Hakimi algorithm
 isGraphicalSequence :: DegreeSequence -> Bool
 isGraphicalSequence (DegreeSequence []) = True
 isGraphicalSequence (DegreeSequence (x:xs))
@@ -37,18 +53,19 @@ isGraphicalSequence (DegreeSequence (x:xs))
         where seq' = subtract 1 <$> take x xs ++ drop x xs
 
 -- | Tell if a 'DegreeSequence' is a Directed Graphic
--- | A @Directed Graphic@ is a Degree Sequence for wich a 'DGraph' exists
+--
+-- A @Directed Graphic@ is a Degree Sequence for which a 'DGraph' exists
 -- TODO: Kleitman–Wang | Fulkerson–Chen–Anstee theorem algorithms
 isDirectedGraphic :: DegreeSequence -> Bool
 isDirectedGraphic = undefined
 
 -- | Tell if a 'DegreeSequence' holds the Handshaking lemma, that is, if the
--- | number of vertices with odd degree is even
+-- number of vertices with odd degree is even
 holdsHandshakingLemma :: DegreeSequence -> Bool
 holdsHandshakingLemma = even . length . filter odd . unDegreeSequence
 
--- | Get the corresponding 'UGraph' of a 'DegreeSequence'
--- | If the 'DegreeSequence' is not graphical (see 'isGraphicalSequence') the
--- | result is Nothing
+-- | Get the corresponding 'UGraph' of a 'DegreeSequence'. If the
+-- 'DegreeSequence' is not graphical (see 'isGraphicalSequence') the result is
+-- Nothing
 fromGraphicalSequence :: DegreeSequence -> Maybe (UGraph Int ())
 fromGraphicalSequence = undefined
